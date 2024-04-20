f840fb689dd896931d477ed15d6d44bd6198ac89
David Furlong <dvfurlong@googlemail.com>
2024-04-20 11:42:14 +0200

    fix: add timer

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -5,6 +5,17 @@ import { LikeIcon, RecastIcon, ReplyIcon } from "./icons";
 import * as fs from "node:fs/promises";
 import * as path from "node:path";
 
+function getTimer(name: string) {
+  const dstart = new Date().getTime();
+
+  return () => {
+    const dend = new Date().getTime();
+
+    const diffMs = dend - dstart;
+    console.info(`timer ${name} measured ${diffMs} miliseconds`);
+  };
+}
+
 const pageSize = 6;
 
 export const runtime = "nodejs";
@@ -27,9 +38,11 @@ async function fetchSearchResult({
   page: number;
 }) {
   try {
+    const searchCasterTimer = getTimer("searchcaster");
     const res = await fetch(
       `https://searchcaster.xyz/api/search?text=${q}&count=${count}&page=${page}`
     );
+    searchCasterTimer();
     const resJson = (await res.json()) as {
       casts: Array<SearchCasterCast>;
     };
@@ -73,46 +86,211 @@ async function fetchSearchResult({
 }
 
 const frameHandler = frames(async (ctx) => {
-  const counter = ctx.message
-    ? ctx.searchParams.op === "+"
-      ? ctx.state.counter + 1
-      : ctx.state.counter === 1
-      ? ctx.state.counter
-      : ctx.state.counter - 1
-    : ctx.state.counter;
+  try {
+    const counter = ctx.message
+      ? ctx.searchParams.op === "+"
+        ? ctx.state.counter + 1
+        : ctx.state.counter === 1
+        ? ctx.state.counter
+        : ctx.state.counter - 1
+      : ctx.state.counter;
 
-  const [interRegularFontData, interBoldFontData] = await Promise.all([
-    interRegularFont,
-    interBoldFont,
-  ]);
+    const [interRegularFontData, interBoldFontData] = await Promise.all([
+      interRegularFont,
+      interBoldFont,
+    ]);
 
-  const query = ctx?.searchParams.query ?? "";
-  const results = ctx.state.results;
-  let newResults = results;
-  if (!results[counter]) {
-    newResults = await fetchSearchResult({
-      q: query,
-      page: Math.floor(counter / pageSize),
-      count: pageSize,
-    });
-  }
+    const query = ctx?.searchParams.query ?? "";
+    const results = ctx.state.results;
+    let newResults = results;
+    if (!results[counter]) {
+      newResults = await fetchSearchResult({
+        q: query,
+        page: Math.floor(counter / pageSize),
+        count: pageSize,
+      });
+    }
 
-  const currentResult = newResults[counter];
-  if (!currentResult && counter === 1) {
-    // errorframe
+    const currentResult = newResults[counter];
+    if (!currentResult && counter === 1) {
+      // errorframe
+      return {
+        image: (
+          <div tw="flex flex-col">
+            <div tw="flex">
+              Couldn't find Farcaster casts that match "{query}"
+            </div>
+          </div>
+        ),
+        buttons: [
+          <Button action="post" target={`?query=${query}`}>
+            Retry
+          </Button>,
+        ],
+        accepts: [
+          {
+            id: "xmtp",
+            version: "vNext",
+          },
+          {
+            id: "farcaster",
+            version: "vNext",
+          },
+        ],
+        // initial frame can't have state
+        ...(ctx.request.method === "POST"
+          ? { state: { counter: counter, results: newResults } }
+          : {}),
+      };
+    }
+    if (!currentResult && newResults.length) {
+      // errorframe
+      return {
+        image: (
+          <div tw="flex flex-col">
+            <div tw="flex">No more results</div>
+          </div>
+        ),
+        buttons: [
+          <Button action="post" target={`?query=${query}`}>
+            Retry
+          </Button>,
+        ],
+        accepts: [
+          {
+            id: "xmtp",
+            version: "vNext",
+          },
+          {
+            id: "farcaster",
+            version: "vNext",
+          },
+        ],
+        // initial frame can't have state
+        ...(ctx.request.method === "POST"
+          ? { state: { counter: counter, results: newResults } }
+          : {}),
+      };
+    }
+    if (!currentResult) {
+      // errorframe
+      return {
+        image: (
+          <div tw="flex flex-col">
+            <div tw="flex">Beep boop frame error</div>
+          </div>
+        ),
+        buttons: [
+          <Button
+            action="post"
+            target={{ pathname: "/", query: { op: "-", query: query } }}
+          >
+            Prev
+          </Button>,
+          <Button action="post" target={`?query=${query}`}>
+            Retry
+          </Button>,
+        ],
+        accepts: [
+          {
+            id: "xmtp",
+            version: "vNext",
+          },
+          {
+            id: "farcaster",
+            version: "vNext",
+          },
+        ],
+        // initial frame can't have state
+        ...(ctx.request.method === "POST"
+          ? { state: { counter: counter, results: newResults } }
+          : {}),
+      };
+    }
+
     return {
       image: (
-        <div tw="flex flex-col">
-          <div tw="flex">
-            Couldn't find Farcaster casts that match "{query}"
+        <div tw="flex flex-col p-8 h-full w-full bg-slate-100">
+          <div tw="flex text-slate-600 text-3xl">
+            Searching Farcaster casts for "{query}"
+          </div>
+          <div tw="flex border border-slate-300 p-8 rounded-[20px] my-4 grow flex-col bg-white">
+            {currentResult.meta.replyParentUsername?.username ? (
+              <div tw="flex flex-row mb-6 text-3xl text-slate-500">
+                Replying to{" "}
+                <div tw="flex font-bold ml-2">
+                  @{currentResult.meta.replyParentUsername?.username}
+                </div>{" "}
+              </div>
+            ) : null}
+            <div tw="flex flex-row grow">
+              <img
+                src={currentResult.meta.avatar}
+                tw="h-24 w-24 rounded-full mr-6"
+              />
+              <div tw="flex flex-col grow flex-wrap">
+                <div tw="flex flex-row">
+                  <div tw="flex font-bold">
+                    {currentResult.meta.displayName}
+                  </div>{" "}
+                  <div tw="flex ml-4">@{currentResult.body.username}</div>
+                </div>
+                <div tw="flex mt-4 [overflow-wrap:anywhere] flex-wrap min-w-[0px] flex-shrink">
+                  {currentResult.body.data.text}
+                </div>
+                <div tw="flex mt-4 items-end text-slate-400 grow">
+                  <div tw="flex mt-6 items-center text-slate-400">
+                    <div tw="flex mr-4 items-center">
+                      <div tw="flex w-10">
+                        <LikeIcon />
+                      </div>
+                      <div tw="flex w-12">
+                        {currentResult.meta.reactions.count}
+                      </div>
+                    </div>
+                    <div tw="flex mr-4 items-center">
+                      <div tw="flex w-10">
+                        <RecastIcon />
+                      </div>
+                      <div tw="flex w-12">
+                        {currentResult.meta.recasts.count}
+                      </div>
+                    </div>
+                    <div tw="flex mr-4 items-center">
+                      <div tw="flex w-10">
+                        <ReplyIcon />
+                      </div>
+                      <div tw="flex w-12">
+                        {currentResult.meta.numReplyChildren}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div tw="flex text-slate-600 text-3xl">
+            Cast {counter} of{" "}
+            {Object.keys(newResults).length >= pageSize
+              ? "many"
+              : Object.keys(newResults).length}
           </div>
         </div>
       ),
-      buttons: [
-        <Button action="post" target={`?query=${query}`}>
-          Retry
-        </Button>,
-      ],
+      imageOptions: {
+        fonts: [
+          {
+            name: "Inter",
+            data: interRegularFontData,
+            weight: 400,
+          },
+          {
+            name: "Inter",
+            data: interBoldFontData,
+            weight: 700,
+          },
+        ],
+      },
       accepts: [
         {
           id: "xmtp",
@@ -123,49 +301,6 @@ const frameHandler = frames(async (ctx) => {
           version: "vNext",
         },
       ],
-      // initial frame can't have state
-      ...(ctx.request.method === "POST"
-        ? { state: { counter: counter, results: newResults } }
-        : {}),
-    };
-  }
-  if (!currentResult && newResults.length) {
-    // errorframe
-    return {
-      image: (
-        <div tw="flex flex-col">
-          <div tw="flex">No more results</div>
-        </div>
-      ),
-      buttons: [
-        <Button action="post" target={`?query=${query}`}>
-          Retry
-        </Button>,
-      ],
-      accepts: [
-        {
-          id: "xmtp",
-          version: "vNext",
-        },
-        {
-          id: "farcaster",
-          version: "vNext",
-        },
-      ],
-      // initial frame can't have state
-      ...(ctx.request.method === "POST"
-        ? { state: { counter: counter, results: newResults } }
-        : {}),
-    };
-  }
-  if (!currentResult) {
-    // errorframe
-    return {
-      image: (
-        <div tw="flex flex-col">
-          <div tw="flex">Beep boop frame error</div>
-        </div>
-      ),
       buttons: [
         <Button
           action="post"
@@ -173,141 +308,28 @@ const frameHandler = frames(async (ctx) => {
         >
           Prev
         </Button>,
-        <Button action="post" target={`?query=${query}`}>
-          Retry
+        <Button
+          action="post"
+          target={{ pathname: "/", query: { op: "+", query: query } }}
+        >
+          Next
         </Button>,
-      ],
-      accepts: [
-        {
-          id: "xmtp",
-          version: "vNext",
-        },
-        {
-          id: "farcaster",
-          version: "vNext",
-        },
+        <Button
+          action="link"
+          target={`https://warpcast.com/${currentResult.body.username}/${currentResult.merkleRoot}`}
+        >
+          Open Cast
+        </Button>,
       ],
       // initial frame can't have state
       ...(ctx.request.method === "POST"
         ? { state: { counter: counter, results: newResults } }
         : {}),
     };
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
-
-  return {
-    image: (
-      <div tw="flex flex-col p-8 h-full w-full bg-slate-100">
-        <div tw="flex text-slate-600 text-3xl">
-          Searching Farcaster casts for "{query}"
-        </div>
-        <div tw="flex border border-slate-300 p-8 rounded-[20px] my-4 grow flex-col bg-white">
-          {currentResult.meta.replyParentUsername?.username ? (
-            <div tw="flex flex-row mb-6 text-3xl text-slate-500">
-              Replying to{" "}
-              <div tw="flex font-bold ml-2">
-                @{currentResult.meta.replyParentUsername?.username}
-              </div>{" "}
-            </div>
-          ) : null}
-          <div tw="flex flex-row grow">
-            <img
-              src={currentResult.meta.avatar}
-              tw="h-24 w-24 rounded-full mr-6"
-            />
-            <div tw="flex flex-col grow flex-wrap">
-              <div tw="flex flex-row">
-                <div tw="flex font-bold">{currentResult.meta.displayName}</div>{" "}
-                <div tw="flex ml-4">@{currentResult.body.username}</div>
-              </div>
-              <div tw="flex mt-4 [overflow-wrap:anywhere] flex-wrap min-w-[0px] flex-shrink">
-                {currentResult.body.data.text}
-              </div>
-              <div tw="flex mt-4 items-end text-slate-400 grow">
-                <div tw="flex mt-6 items-center text-slate-400">
-                  <div tw="flex mr-4 items-center">
-                    <div tw="flex w-10">
-                      <LikeIcon />
-                    </div>
-                    <div tw="flex w-12">
-                      {currentResult.meta.reactions.count}
-                    </div>
-                  </div>
-                  <div tw="flex mr-4 items-center">
-                    <div tw="flex w-10">
-                      <RecastIcon />
-                    </div>
-                    <div tw="flex w-12">{currentResult.meta.recasts.count}</div>
-                  </div>
-                  <div tw="flex mr-4 items-center">
-                    <div tw="flex w-10">
-                      <ReplyIcon />
-                    </div>
-                    <div tw="flex w-12">
-                      {currentResult.meta.numReplyChildren}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div tw="flex text-slate-600 text-3xl">
-          Cast {counter} of{" "}
-          {Object.keys(newResults).length >= pageSize
-            ? "many"
-            : Object.keys(newResults).length}
-        </div>
-      </div>
-    ),
-    imageOptions: {
-      fonts: [
-        {
-          name: "Inter",
-          data: interRegularFontData,
-          weight: 400,
-        },
-        {
-          name: "Inter",
-          data: interBoldFontData,
-          weight: 700,
-        },
-      ],
-    },
-    accepts: [
-      {
-        id: "xmtp",
-        version: "vNext",
-      },
-      {
-        id: "farcaster",
-        version: "vNext",
-      },
-    ],
-    buttons: [
-      <Button
-        action="post"
-        target={{ pathname: "/", query: { op: "-", query: query } }}
-      >
-        Prev
-      </Button>,
-      <Button
-        action="post"
-        target={{ pathname: "/", query: { op: "+", query: query } }}
-      >
-        Next
-      </Button>,
-      <Button
-        action="link"
-        target={`https://warpcast.com/${currentResult.body.username}/${currentResult.merkleRoot}`}
-      >
-        Open Cast
-      </Button>,
-    ],
-    // initial frame can't have state
-    ...(ctx.request.method === "POST"
-      ? { state: { counter: counter, results: newResults } }
-      : {}),
-  };
 });
 
 export const GET = frameHandler;
